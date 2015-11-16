013a3a6a16b845ea4d236f0bea7237ca563684c3
Tiago <tiago_908@hotmail.com>
2015-11-16 21:23:59 +0000

    init commit

---
+++ Nightpath_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nightpath_project.Startup))]
+namespace Nightpath_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
